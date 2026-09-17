0601d302aa20013a9e17d8b4483f0b4ce024790d
Takezo909 <kenshee.dampilag@gmail.com>
2026-09-17 22:01:09 +0800

    Added a prototype chesspiece  movement(Only shows up on the console for now)

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const game = new Chess();
+
+let selectedSquare = null;
+
+console.log(game.fen());
+
 const board = document.getElementById("chessboard");
 
 const pieces = [
@@ -16,11 +22,39 @@ for (let row = 0; row < 8; row++) {
 
         const square = document.createElement("div");
 
+        const squareName = String.fromCharCode(97 + col) + (8 - row);
+        square.dataset.square = squareName;
+        square.addEventListener("click", handleSquareClick);
+
         if ((row + col) % 2 === 0) {
             square.classList.add("light");
         } else {
             square.classList.add("dark");
         }
+
+        function handleSquareClick(event) {
+            const square = event.currentTarget;
+            const squareName = square.dataset.square;
+
+            if (selectedSquare === null) {
+                selectedSquare = squareName;
+                console.log("Selected:", selectedSquare);
+            } else {
+
+                const move = game.move({
+                    from: selectedSquare,
+                    to: squareName
+            });
+
+            if (move) {
+                console.log("Legal move:", move);
+            } else {
+                console.log("Illegal move");
+            }
+
+            selectedSquare = null;
+        }
+}
 
         const piece = pieces[row * 8 + col];
 
