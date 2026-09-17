e70b4e6ad6a83d35bee40daef3baddc5f510df3e
Takezo909 <kenshee.dampilag@gmail.com>
2026-09-17 21:23:43 +0800

    First progress

---
+++ app.js
@@ -0,0 +1,16 @@
+const board = document.getElementById("chessboard");
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+
+        const square = document.createElement("div");
+
+        if ((row + col) % 2 === 0) {
+            square.classList.add("light");
+        } else {
+            square.classList.add("dark");
+        }
+
+        board.appendChild(square);
+    }
+}
